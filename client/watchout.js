0d9ed5a8fab477486ca21d2fefa83f863375e5f9
derektliu <liu.derekt@gmail.com>
2016-08-05 17:10:56 -0700

    Draggable player!!!

--- client/watchout.js
+++ client/watchout.js
@@ -4,6 +4,9 @@ var allAsteroids = [];
 // set how often the asteroids move
 var interval = 1500;
 var speed = 750;
+var numAsteroids = 30;
+
+var player = { cx: 350, cy: 225, r: 10 };
 
 // create asteroids for board
 var createAsteroids = function(n) {
@@ -13,8 +16,6 @@ var createAsteroids = function(n) {
   }
 };
 
-var svg = d3.select('svg');
-
 // Board size: x="700" y="450"
 // randomize position of current asteroid
 var randomize = function(asteroid) {
@@ -23,36 +24,45 @@ var randomize = function(asteroid) {
   return asteroid;
 };
 
-var player = function () {
-  svg.cx = 350;
-  svg.cy = 225;
-};
-
-createAsteroids(30);
+createAsteroids(numAsteroids);
 // console.log('allAsteroids', allAsteroids);
 
-var selection = svg.selectAll('circle').data(allAsteroids);
-// console.log('selection', selection);
+var svg = d3.select('svg');
+var selectEnemy = svg.selectAll('enemy').data(allAsteroids);
+var selectPlayer = svg.selectAll('player').data([player]);
+
+// Create drag behavior
+var drag = d3.behavior.drag()
+.on('dragstart', function() { selectPlayer.style('fill', 'red'); })
+.on('drag', function() { selectPlayer.attr('cx', d3.event.x).attr('cy', d3.event.y); })
+.on('dragend', function() { selectPlayer.style('fill', 'orange'); });
 
 // Updates coordinates for existing asteroids
 var updateCoordinates = function() {
-
-  selection
+  selectEnemy
   .transition().duration(speed)
   .attr('cx', function(d) { return Math.random() * 700; })
   .attr('cy', function(d) { return Math.random() * 450; });
-
 };
 
-selection
+// // Create Enemies
+selectEnemy
 .enter().append('circle')
 .attr('class', 'enemy')
 .attr('cx', function(d) { return d.cx; })
 .attr('cy', function(d) { return d.cy; })
 .attr('r', function(d) { return d.r; })
 .attr('href', 'asteroid.png');
-
-// selection
-// .exit();
-
+// Randomize locations of enemies
 setInterval(updateCoordinates, interval);
+
+// Create Player
+selectPlayer
+.enter()
+.append('circle')
+.attr('class', 'player')
+.attr('cx', function(d) { return d.cx; })
+.attr('cy', function(d) { return d.cy; })
+.attr('r', function(d) { return d.r; })
+.call(drag)
+.style('fill', 'orange');
